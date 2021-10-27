1622db64a0f1e8675cd884d8b209df2e2eef192b
Tom Greenwood <thomas.greenwood@kainos.com>
2021-10-27 14:52:56 +0100

    basic install on debian with plugins

---
+++ nvim-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo apt-get install nvim
+
+mkdir -p ~/.config/nvim/autoload
+mkdir -p ~/.config/nvim/plugged
+mkdir -p ~/.config/nvim/undodir
+sudo chmod 777 ~/.config/nvim
+
+curl -fLo ~/.config/nvim/autoload/plug.vim \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.config/nvim/init.vim \
+    https://raw.githubusercontent.com/tomgreenwood10/dev/master/init.vim
+
+nvim -c PlugInstall +qa
+sudo apt-get install -y cmake
+python3 ~/.config/nvim/plugged/YouCompleteMe/install.py --clangd-completer
+
