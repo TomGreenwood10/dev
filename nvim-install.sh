89e9ac238ad357bbaa7392ded5dd82412037d1b0
Tom Greenwood <thomas.greenwood@kainos.com>
2021-10-28 14:12:25 +0100

    install as per github instructions

--- nvim-install.sh
+++ nvim-install.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-sudo apt-get install -y python3-dev
-sudo apt-get install -y python3-pip
-sudo apt-get install -y cmake
-sudo apt-get install -y vim-nox
-
-sudo apt-get install -y neovim
-pip3 install pynvim --upgrade
-
 mkdir -p ~/.config/nvim/autoload
 mkdir -p ~/.config/nvim/plugged
 mkdir -p ~/.config/nvim/undodir
@@ -19,5 +11,9 @@ curl -fLo ~/.config/nvim/init.vim \
     https://raw.githubusercontent.com/tomgreenwood10/dev/master/init.vim
 
 nvim -c PlugInstall +qa
-python3 ~/.config/nvim/plugged/YouCompleteMe/install.py --clangd-completer
+
+sudo apt install build-essential cmake vim-nox python3-dev
+sudo apt install -y mono-complete golang nodejs default-jdk npm
+
+python3 ~/.config/nvim/plugged/YouCompleteMe/install.py --all
 
