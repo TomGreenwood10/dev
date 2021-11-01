a7e733e6c0040dc5754134534d6e136a512e3767
Tom Greenwood <30959879+TomGreenwood10@users.noreply.github.com>
2021-11-01 15:49:08 +0000

    removed nvm source

--- nvim-install.sh
+++ nvim-install.sh
@@ -5,7 +5,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node
-source ~/.nvm/nvm.sh
 npm i -g pyright
 
 mkdir -p ~/.config/nvim/autoload
