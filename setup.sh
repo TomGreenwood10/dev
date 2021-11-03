99c6cec3d904bd160565da89271406a4d3bd030c
Tom Greenwood <thomas.greenwood@kainos.com>
2021-11-03 10:06:07 +0000

    always install nvim

--- setup.sh
+++ setup.sh
@@ -8,16 +8,18 @@ echo Setting up environment ...
 read -p "Enter git username: " git_username
 read -p "Enter git email: " git_email
 read -p "Add ZScaler certificate? (y/n): " zscaler_cert
-read -p "Install and configure neovim? (y/n): " neovim
 read -p "Install desktop GUI? (y/n): " desktop
 
 # Main installation script
-sudo apt update
-sudo apt upgrade
+sudo apt-get update
+sudo dpkg --configure -a
+sudo apt-get upgrade -y
 
 git config --global user.name "$git_username"
 git config --global user.email "$git_email"
 
 source scripts/misc-install.sh
+source scripts/nvim-install.sh
 if [ "$zscaler_cert" == "y" ]; then source scripts/certification-setup.sh; fi
-if [ "$neovim" == "y" ]; then source scripts/nvim-install.sh
+if [ "$desktop" == "y" ]; then sudo apt-get install -y ubuntu-desktop; fi
+
