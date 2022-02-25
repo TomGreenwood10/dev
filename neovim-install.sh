17dd7ab155738e06f773b9d6a392c2568b44206f
Tom Greenwood <tomgreenwood10@gmail.com>
2022-02-25 08:38:13 +0000

    Scala install removed

--- neovim-install.sh
+++ neovim-install.sh
@@ -32,23 +32,28 @@ curl -fLo ~/.config/nvim/autoload/plug.vim \
 
 nvim -c PlugInstall +qa
 
-# Install scala support (installs coursier which in-turn installs sbt, java, 
-# scala etc.):
-
-# Install coursier launcher (cs)
-curl -fLo cs https://git.io/coursier-cli-"$(uname | tr LD ld)"
-chmod +x cs
-./cs install cs
-rm cs
-export PATH="$PATH:/home/vagrant/.local/share/coursier/bin"
-
-# Add path to bashrc to work on future shells
-echo '' >> ~/.bashrc
-echo '# Path to coursier (cs) executable directory' >> ~/.bashrc
-echo 'export PATH="$PATH:/home/vagrant/.local/share/coursier/bin"' >> ~/.bashrc
-echo '' >> ~/.bashrc
-
-# Install other tools with cs (this prompts user for multiple inputs)
-cs setup
-cs install metals
-
+# =============================================================================
+# Removed scala install as it's buggy and interfears with pip install pyspark
+# Left here for future reference / modification
+# -----------------------------------------------------------------------------
+# 
+# # Install scala support (installs coursier which in-turn installs sbt, java, 
+# # scala etc.):
+# 
+# # Install coursier launcher (cs)
+# curl -fLo cs https://git.io/coursier-cli-"$(uname | tr LD ld)"
+# chmod +x cs
+# ./cs install cs
+# rm cs
+# export PATH="$PATH:/home/vagrant/.local/share/coursier/bin"
+# 
+# # Add path to bashrc to work on future shells
+# echo '' >> ~/.bashrc
+# echo '# Path to coursier (cs) executable directory' >> ~/.bashrc
+# echo 'export PATH="$PATH:/home/vagrant/.local/share/coursier/bin"' >> ~/.bashrc
+# echo '' >> ~/.bashrc
+# 
+# # Install other tools with cs (this prompts user for multiple inputs)
+# cs setup
+# cs install metals
+# ============================================================================
